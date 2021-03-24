daf91b721d93ce6091adb1c63cc7079e2ab0d7f5
kin.kim <im@kin.kim>
2021-03-24 10:54:32 +0800

    用户变更

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -61,7 +61,7 @@ const actions = {
         }
 
         // const { roles, name, avatar, introduction } = data
-        const { roles, username, password, accountNonExpired } = data
+        const { roles, username, avatar, introduction } = data
 
         // roles must be a non-empty array
         if (!roles || roles.length <= 0) {
@@ -70,8 +70,8 @@ const actions = {
 
         commit('SET_ROLES', roles)
         commit('SET_NAME', username)
-        commit('SET_AVATAR', password)
-        commit('SET_INTRODUCTION', accountNonExpired)
+        commit('SET_AVATAR', avatar)
+        commit('SET_INTRODUCTION', introduction)
         resolve(data)
       }).catch(error => {
         reject(error)
