ef411b067bcbbdd6f8fbd2db3128cf4c65deaf18
kin.kim <im@kin.kim>
2021-09-18 10:12:09 +0800

    init:20210918

---
+++ src/test/java/kim/kin/promgr/KkPromgrApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.kin.promgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkPromgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
